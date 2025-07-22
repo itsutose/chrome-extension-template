522ddd8ffa94bfd4d924b4f415f24018e8b5e02d
itsutose <itsutose5@gmail.com>
2025-07-22 18:26:58 +0900

    tsconfig.app.json, vite.config.tsへコメント付与

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,19 @@ import webExtension from 'vite-plugin-web-extension';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // プラグイン設定
   plugins: [
-    react(),
-    webExtension({
-      manifest: 'public/manifest.json',
-      browser: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+    react(),                    // React用プラグイン
+    webExtension({              // Chrome Extension用プラグイン
+      manifest: 'public/manifest.json', // Chrome Extensionのマニフェストファイルのパス
+      browser: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser', // ブラウザのパス
     })
   ],
+
+  // ビルド設定
   build: {
-    sourcemap: 'inline',
-    minify: false,
+    sourcemap: 'inline', // ソースマップを埋め込み、デバッグ時に変換後のJSファイルから元のTSファイルを特定できる
+    minify: false, // コード（変数、関数名）圧縮なし、デバッグ時にコードを読みやすくするため
     rollupOptions: {
       output: {
         sourcemapExcludeSources: false,
